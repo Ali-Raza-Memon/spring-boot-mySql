1f3ed1540399dea1117972d64e74e6ee3bdde37c
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-05-09 01:37:05 +0500

    In mySql,database connection are created with the column in database

---
+++ src/main/java/com/xadmin/SpringBootCrud/controller/SubjectController.java
@@ -0,0 +1,39 @@
+package com.xadmin.SpringBootCrud.controller;
+
+import com.xadmin.SpringBootCrud.bean.Subject;
+import com.xadmin.SpringBootCrud.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SubjectController {
+
+    @Autowired
+    private SubjectService subjectService;
+
+    @RequestMapping("/subjects")
+    public List<Subject> getAllSubjects(){
+
+           return subjectService.getAllSubjects();
+    }
+
+    @RequestMapping( value="/subjects/{id}", method = RequestMethod.PUT)
+    public void updateSubject(@PathVariable String id,@RequestBody Subject subject){
+
+        subjectService.updateSubject(id,subject);
+    }
+
+
+    @RequestMapping( value="/subjects",method = RequestMethod.POST )
+    public void addSubject( @RequestBody Subject subject){
+
+        subjectService.addSubject(subject);
+    }
+
+    public void DeleteSubject(@PathVariable String id ){
+        subjectService.deleteSubject(id);
+    }
+
+}
